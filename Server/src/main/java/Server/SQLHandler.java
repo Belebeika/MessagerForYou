18f54fad8545c183c4326ba05acba7d2fd455796
TheSlavik22 <slavyanka345@mail.ru>
2023-12-15 21:33:09 +1000

    добавление в друзья

--- Server/src/main/java/Server/SQLHandler.java
+++ Server/src/main/java/Server/SQLHandler.java
@@ -1,6 +1,8 @@
 package Server;
 
 import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
 
 public class SQLHandler {
     private static Connection connection;
@@ -22,6 +24,21 @@ public class SQLHandler {
         stmt.execute("CREATE TABLE if not exists 'friends' ('id' INTEGER PRIMARY KEY AUTOINCREMENT, 'user_id' INTEGER NOT NULL, 'friend_id' INTEGER NOT NULL, FOREIGN KEY('user_id') REFERENCES 'users' (id), FOREIGN KEY('friend_id') REFERENCES 'users' (id));");
 
         System.out.println("Таблицы созданы или уже существуют.");
+    }
+
+    public static boolean checknick(String nick) throws SQLException {
+        ResultSet rs = stmt.executeQuery("SELECT id FROM users WHERE nickname = '" + nick + "';");
+        if(rs.next()){
+            return true;
+        }
+        return false;
+    }
+
+    public  static void addfriend(String usernick, String friendnick) throws SQLException {
+        stmt.executeUpdate("INSERT INTO friends (user_id, friend_id) VALUES (user.id, friend.id) JOIN " +
+                "users AS user ON user.nickname = '" + usernick + "' JOIN users AS friend ON friend.nickname = '" + friendnick + "';");
+        stmt.executeUpdate("INSERT INTO friends (user_id, friend_id) VALUES (friend.id, user.id) JOIN " +
+                "users AS user ON user.nickname = '" + usernick + "' JOIN users AS friend ON friend.nickname = '" + friendnick + "';");
     }
     public static String getNickByLoginAndPassword(String login, String password) {
         try {
@@ -60,5 +77,19 @@ public class SQLHandler {
         } catch (SQLException e) {
             e.printStackTrace();
         }
+    }
+
+    public  static List<String> getFriends(String nickname){
+        List<String> str = new ArrayList<>();
+        try {
+            ResultSet rs = stmt.executeQuery("SELECT friend.nickname FROM users AS user JOIN users AS friend " +
+                    "JOIN friends ON user.id = friends.user_id AND user.nickname = '" + nickname + "' AND friend.id = friends.friend_id;");
+            while (rs.next()){
+                str.add(rs.getString(1));
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+        return str;
     }
 }
